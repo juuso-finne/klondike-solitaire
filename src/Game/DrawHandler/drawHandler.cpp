c3db8f84bb00cd3345d76df4d4ce8df433a1289c
juuso-finne <juuso.f@gmail.com>
2026-05-27 19:20:25 +0300

    Add DrawHandler

---
+++ src/Game/DrawHandler/drawHandler.cpp
@@ -0,0 +1,21 @@
+#include "drawHandler.h"
+#include "../game.h"
+
+DrawHandler::DrawHandler(Game *aGame)
+{
+    game = aGame;
+    cardSpriteSheet = LoadTexture("assets/graphics/card_spritesheet.png");
+}
+
+void DrawHandler::Draw()
+{
+    DrawGame();
+}
+
+void DrawHandler::DrawGame()
+{
+    for (Column col : game->columns)
+    {
+        col.Draw(game->settings.cardStagger, cardSpriteSheet);
+    }
+}
